c665a63ff9b4b4ca815e36f39acaff8cbc9c12b1
William Lowrimore <wlowrimore@gmail.com>
2022-12-21 09:42:09 -0600

    sets up the expenses output and begins build out for the expenses components that are used in the ExpensesOutput component

---
+++ components/ExpensesOutput/ExpensesSummary.js
@@ -0,0 +1,16 @@
+import { Text, View, StyleSheet } from "react-native";
+
+function ExpensesSummary({ periodName, expenses }) {
+  const expensesSum = expenses.reduce((sum, expense) => {
+    return sum + expense.amount;
+  }, 0);
+
+  return (
+    <View>
+      <Text>{periodName}</Text>
+      <Text>${expensesSum.toFixed(2)}</Text>
+    </View>
+  );
+}
+
+export default ExpensesSummary;
